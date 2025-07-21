3f98e4629e981327ee42dd0e666c276bbdaa43b4
Jlquintero97 <208868415+97JoseLuis@users.noreply.github.com>
2025-07-21 20:37:18 +0200

    actualizando

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,10 @@ const env = process.env.NODE_ENV || 'development';
 require('dotenv').config({ path: `.env.${env}` });
 
 // Middlewares
-app.use(cors());
+app.use(cors({
+  origin: ['https://venta-coches.vercel.app'],
+  credentials: true,
+}));
 app.use(express.json());
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
